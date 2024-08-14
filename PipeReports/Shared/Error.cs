68b982746cd44e698b33616ffa0237f772c1bdcc
Michael Lechowski <M02576@uniper.energy>
2024-08-14 15:58:09 +0200

    shared results handling

---
+++ PipeReports/Shared/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace PipeReports.API.Shared;
+
+public sealed record Error(string Code, string? Message = null)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+}
---
+++ PipeReports/Shared/Result.cs
@@ -0,0 +1,29 @@
+﻿namespace PipeReports.API.Shared;
+
+public class Result
+{
+    protected Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None ||
+            !isSuccess && error == Error.None)
+        {
+            throw new InvalidOperationException();
+        }
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+
+    public static Result Failure(Error error) => new(false, error);
+
+    public static Result<T> Success<T>(T value) => new(value, true, Error.None);
+
+    public static Result<T> Failure<T>(Error error) => new(default, false, error);
+}
