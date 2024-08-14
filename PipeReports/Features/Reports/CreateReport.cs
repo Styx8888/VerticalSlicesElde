d0c86b5e1d4243cf71125483a90b6f2d69c1cd38
Michael Lechowski <M02576@uniper.energy>
2024-08-14 15:58:00 +0200

    report feature

---
+++ PipeReports/Features/Reports/CreateReport.cs
@@ -0,0 +1,81 @@
+﻿using Carter;
+using FluentValidation;
+using MediatR;
+using PipeReports.API.Contracts;
+using PipeReports.API.Database;
+using PipeReports.API.Entities;
+using PipeReports.API.Shared;
+
+namespace PipeReports.API.Features.Reports;
+
+public static class CreateReport
+{
+    public sealed record Command() : IRequest<Result<int>>
+    {
+        public string Client { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Client).NotEmpty().WithMessage("Client property must have a value.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email property must have a value.");
+        }
+    }
+
+    internal sealed class Handler : IRequestHandler<Command, Result<int>>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IValidator<Command> _validator;
+
+        public Handler(ApplicationDbContext dbContext, IValidator<Command> validator)
+        {
+            _dbContext = dbContext;
+            _validator = validator;
+        }
+
+        public async Task<Result<int>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var validationResult = _validator.Validate(request);
+            if (!validationResult.IsValid)
+                return Result.Failure<int>(new Error("CreateReport.Validation", validationResult.ToString()));
+
+            var report = new Report()
+            {
+                Client = request.Client,
+                Email = request.Email,
+            };
+
+            await _dbContext.AddAsync(report);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Result.Success(report.Id);
+        }
+    }
+}
+
+public class CreateReportEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/reports", async (CreateReportRequest request, ISender sender) =>
+        {
+            var command = new CreateReport.Command()
+            {
+                Client = request.Client,
+                Email = request.Email,
+            };
+
+            var result = await sender.Send(command);
+
+            if (result.IsFailure)
+            {
+                return Results.BadRequest(result.Error);
+            }
+
+            return Results.Ok(result.Value);
+        });
+    }
+}
---
+++ PipeReports/Features/Reports/GetReport.cs
@@ -0,0 +1,59 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PipeReports.API.Contracts;
+using PipeReports.API.Database;
+using PipeReports.API.Shared;
+
+namespace PipeReports.API.Features.Reports;
+
+public static class GetReport
+{
+    public sealed record Query(int Id) : IRequest<Result<ReportResponse>>;
+
+    internal sealed class Handler : IRequestHandler<Query, Result<ReportResponse>>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public Handler(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Result<ReportResponse>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var reportResponse = await _dbContext
+                .Reports
+                .AsNoTracking()
+                .Select(x => new ReportResponse(x.Id,
+                                                x.Client,
+                                                x.Email)
+                )
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
+
+            if (reportResponse is null)
+                return Result.Failure<ReportResponse>(new Error("GetReport.NotFound", $"Report with id = {request.Id} not found."));
+            return reportResponse;
+        }
+    }
+}
+
+public class GetReportEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/reports/{id:int}", async (int id, ISender sender) =>
+        {
+            var query = new GetReport.Query(id);
+
+            var result = await sender.Send(query);
+
+            if (result.IsFailure)
+            {
+                return Results.NotFound(result.Error);
+            }
+
+            return Results.Ok(result.Value);
+        });
+    }
+}
